1b5e0b43c38cd7929f95f9dd79931e17f5a07473
kotsiopoulosn97 <kotsiopoulosn@gmail.com>
2023-05-09 13:20:43 +0300

    hand game

--- main.py
+++ main.py
@@ -1,41 +1,80 @@
 import cv2
 import mediapipe as mp
+
+# initialize the camera
+camera = cv2.VideoCapture(0)  # 0 is the default camera index
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+# initialize the Mediapipe drawing utility
 mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-#  webcam input:
-cap = cv2.VideoCapture(0)
-with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      continue
+# define the size and position of the square
+square_size = 50
+square_x = 295
+square_y = 215
 
+# loop over the frames from the camera
+while True:
+    # read a frame from the camera
+    ret, image = camera.read()
 
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
+    # flip the image horizontally for a mirror effect
+    image = cv2.flip(image, 1)
 
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
-cap.release()
+    # convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+    # detect hands in the image
+    with mp_hands.Hands(
+            static_image_mode=False,
+            max_num_hands=1,
+            min_detection_confidence=0.7) as hands:
+        results = hands.process(gray)
 
+        # get the landmarks of the hand
+        if results.multi_hand_landmarks:
+            hand_landmarks = results.multi_hand_landmarks[0]
+
+            # draw the landmarks on the image
+            mp_drawing.draw_landmarks(
+                image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+            # get the top points of the index, thumb, and middle fingers
+            index_top = tuple(
+                map(int, hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image.shape[1],
+                    hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image.shape[0]))
+            thumb_top = tuple(
+                map(int, hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x * image.shape[1],
+                    hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y * image.shape[0]))
+            middle_top = tuple(
+                map(int, hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x * image.shape[1],
+                    hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y * image.shape[0]))
+
+            # check if any of the top points is inside the square
+            if index_top[0] >= square_x and index_top[0] <= square_x + square_size and index_top[1] >= square_y and index_top[1] <= square_y + square_size:
+                square_x = index_top[0] - square_size // 2
+                square_y = index_top[1] - square_size // 2
+            elif thumb_top[0] >= square_x and thumb_top[0] <= square_x + square_size and thumb_top[1] >= square_y and thumb_top[1] <= square_y + square_size:
+                square_x = thumb_top[0] - square_size // 2
+                square_y = thumb_top[1] - square_size // 2
+            elif middle_top[0] >= square_x and middle_top[0] <= square_x + square_size and middle_top[1] >= square_y and middle_top[1] <= square_y + square_size:
+                square_x = middle_top[0] - square_size // 2
+                square_y = middle_top[1] - square_size // 2
+ # draw the square on the image
+                cv2.rectangle(
+                    image, (square_x, square_y),
+                    (square_x + square_size, square_y + square_size),
+                    (0, 255, 0), 2)
+
+    # display the image
+    cv2.imshow("Hand Tracking Game", image)
+
+    # check if the user wants to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# release the camera and close the window
+camera.release()
+cv2.destroyAllWindows()
